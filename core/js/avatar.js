68e6bfb03cab06cb26e39ec5dc61fcf2b8e13fe6
Roeland Jago Douma <roeland@famdouma.nl>
2015-07-30 20:31:27 +0200

    Let javascript handle avatar
    This allows us to use the devicePixelRatio.

--- core/js/avatar.js
+++ core/js/avatar.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	if (OC.currentUser) {
 		// Personal settings
 		$('#avatar .avatardiv').avatar(OC.currentUser, 128);
+		// Top avatar
+		$('#settings .avatardiv').avatar(OC.currentUser, 32, undefined, true);
 	}
 	// User settings
 	$.each($('td.avatar .avatardiv'), function(i, element) {
